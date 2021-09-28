50d7e2375deb73fdc82d19f32cc895030174f0af
Jose Vicente Nunez <josevnz@kodegeek.com>
2021-09-28 08:50:31 -0400

    Commit for PluralSight Project

--- src/deadlined_reminders.py
+++ src/deadlined_reminders.py
@@ -1,5 +1,8 @@
-from abc import ABCMeta, abstractmethod
+from abc import ABC, ABCMeta, abstractmethod
 from collections.abc import Iterable
+from datetime import datetime
+
+from dateutil.parser import parse
 
 
 class DeadlinedMetaReminder(Iterable):
@@ -10,3 +13,23 @@ class DeadlinedMetaReminder(Iterable):
     @abstractmethod
     def is_due(self):
         pass
+
+
+class DeadlinedReminder(ABC, Iterable):
+
+    @abstractmethod
+    def is_due(self) -> bool:
+        pass
+
+
+class DateReminder(DeadlinedReminder):
+
+    def __init__(self, text: str, date: str):
+        self.date = parse(date, dayfirst=True)
+        self.text = text
+
+    def is_due(self) -> bool:
+        return self.date <= datetime.now()
+
+    def __iter__(self):
+        return iter([self.text, self.date.isoformat()])
